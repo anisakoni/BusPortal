8391a3593f905e68518724b97a47e311df69d1cb
andetazeqiri <andeta.zeqiri@fshnstudent.info>
2025-02-01 21:19:44 +0100

    edit

--- BusPortal.Web/Models/DTO/AddLineViewModel.cs
+++ BusPortal.Web/Models/DTO/AddLineViewModel.cs
@@ -5,6 +5,6 @@
         public string StartCity { get; set; }
         public string DestinationCity { get; set; }
         public string DepartureTimes { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
     }
 }

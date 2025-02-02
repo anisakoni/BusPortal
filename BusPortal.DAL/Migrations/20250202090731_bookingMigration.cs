a1c1d05b8732b86f98d0c3347847e10556edbb35
AnxhelaSokolaj <anxhela.sokolaj@fshnstudent.info>
2025-02-02 10:13:30 +0100

    bookingMigration

---
+++ BusPortal.DAL/Migrations/20250202090731_bookingMigration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusPortal.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class bookingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PaymentId",
+                table: "Bookings",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_PaymentId",
+                table: "Bookings",
+                column: "PaymentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Payments_PaymentId",
+                table: "Bookings",
+                column: "PaymentId",
+                principalTable: "Payments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Payments_PaymentId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_PaymentId",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentId",
+                table: "Bookings");
+        }
+    }
+}
